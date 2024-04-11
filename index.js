054b4b3683b46e1901cbac27e2ea5d4926eb052d
Joy Renee Mugo <mugojoy24@gmail.com>
2024-04-11 16:30:03 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+
+const employee = {
+    name: 'Renee',
+    streetAddress: '20 Beverly Hills'
+}
+
+//console.log(updateEmployeeWithKeyAndValue)
+
+function updateEmployeeWithKeyAndValue(employeeObj, key, value){
+    employeeObj = {...employee}
+    employeeObj[key] = value
+
+    return employeeObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value){
+    employeeObj = employee 
+    employeeObj[key] = value
+
+    return employeeObj
+}
+
+function deleteFromEmployeeByKey(employeeObj, key) {
+    employeeObj = {...employee}
+    delete employeeObj[key]
+
+    return employeeObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObj, key){
+    employeeObj = employee
+    delete employeeObj[key]
+
+    return employeeObj
+}
+
